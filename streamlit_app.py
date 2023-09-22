fc7ae40da25a345d789bbc5e3921d2ad90e3ab3c
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-21 19:26:20 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import io
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+# Load data
+@st.cache
+def load_data():
+    df = pd.read_csv("C:/Users/lwjct/Downloads/Fish.csv")
+    return df
+
+df = load_data()
+
+# Group by species and calculate mean
+group_mean = df.groupby('Species').mean()
+
+# Display the bar chart using Streamlit
+st.bar_chart(group_mean['Weight'])
+
+# Customize the chart's appearance
+st.title("The average weight for each species")
+st.xlabel('Species')
+st.ylabel('Average weight')
+
+
