02734cc75d71faa14658048e9767db2ec053dde7
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-21 20:46:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,12 @@ data = {'Category': ['Math', 'English', 'P.E', 'CS', 'Science'],
 df = pd.DataFrame(data)
 st.title("Student Counts for Each Subject")
 
+# Set labels for x-axis and y-axis using Markdown
+st.markdown("**X-axis Label**: Subject")
+st.markdown("**Y-axis Label**: Students Counts")
+
 # Create a bar plot using st.pyplot
-st.bar_chart(df.set_index('Category')['Values'], use_container_width=True)
+st.bar_chart(df.set_index('Category')['Values'], use_container_width=True,x= "Subject", y = "Students Counts")
 
 
 
